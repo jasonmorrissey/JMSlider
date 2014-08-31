5e7ea5b6c525bd41b6e9f09edeb706b30843013e
Jason Morrissey <me@jasonmorrissey.org>
2014-08-31 15:22:25 +1000

    Reduced opacity of track view

--- JMSlider/Internal/JMSliderConstants.h
+++ JMSlider/Internal/JMSliderConstants.h
@@ -1,15 +1,15 @@
 //  Created by Jason Morrissey - jasonmorrissey.org
 
-#define kJMSideTitleFont [UIFont fontWithName:@"HelveticaNeue-Bold" size:12.]
+#define kJMSideTitleFont [UIFont fontWithName:@"HelveticaNeue-Light" size:11.]
 #define kJMSideTitleColor [UIColor colorWithWhite:0.76 alpha:1.]
 
 #define kJMButtonPadding CGSizeMake(34., 8.)
-#define kJMButtonFont [UIFont fontWithName:@"HelveticaNeue" size:14.]
+#define kJMButtonFont [UIFont fontWithName:@"HelveticaNeue-Light" size:13.]
 #define kJMSliderButtonInvisiblePadding CGSizeMake(40., 30.)
 #define kJMButtonColor [UIColor colorWithWhite:0.7 alpha:1.]
 #define kJMButtonOutlineColor [UIColor colorWithWhite:0. alpha:0.1]
 
-#define kJMSliderTrackColor [UIColor colorWithWhite:0. alpha:0.1]
+#define kJMSliderTrackColor [UIColor colorWithWhite:0.5 alpha:0.2]
 #define kJMSliderTrackEdgeRadius 3.
 #define kJMSliderAnimationCenterButton @"kJMSliderAnimationCenterButton"
 #define kJMSliderAnimationHighlight @"kJMSliderAnimationHighlight"
